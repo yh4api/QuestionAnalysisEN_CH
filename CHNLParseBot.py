b8c83a5e70e5b722c05d8af4d49a98122a561352
yh4api <yh4api@gmail.com>
2016-06-23 09:57:54 +0800

    Add files via upload

--- CHNLParseBot.py
+++ CHNLParseBot.py
@@ -1,5 +1,5 @@
 #-*- coding:utf-8 -*-
-#last updated:2016.06.15
+#last updated:2016.06.21
 import sys, os
 import re
 #default structure: 
@@ -122,6 +122,8 @@ class ChNLParser:
 				ws.append("SPaAcE")
 			elif re_alldigit.match(w.word) != None:
 				fs.append("m_alldigits")
+			elif w.flag.startswith("v") and len(ws) > 1 and ws[-2] == "请":#jieba sometimes pos-tag verb after "請" as "vn" because the result relies on the dictionary heavily.
+				fs.append("v")
 			else:
 				fs.append(w.flag)
 		return ws, fs
@@ -304,7 +306,7 @@ class ChNLParser:
 		elif re.search("(專利|专利)", self.content["target"])!= None:
 			self.content["keywords"].append(self.content["target"])
 			self.content["target"] = "PATENT"
-		elif re.search("(資料|資訊|资讯|资料|文献|文獻)", self.content["target"])!= None:
+		elif re.search("(資料|資訊|资讯|资料|文献|文獻|訊息|讯息)", self.content["target"])!= None:
 			self.content["keywords"].append(self.content["target"])
 			self.content["target"] = "INFORMATION"
 
@@ -318,7 +320,7 @@ class ChNLParser:
 				self.content["target"] = "PAPERS"
 			elif re.search("(專利|专利)", sentence)!= None:
 				self.content["target"] = "PATENT"
-			elif re.search("(资料|资讯|文献|資料|資訊|文獻)", sentence)!= None:
+			elif re.search("(资料|资讯|文献|資料|資訊|文獻|讯息|訊息)", sentence)!= None:
 				self.content["target"] = "INFORMATION"
 
 		tmpkw = []
